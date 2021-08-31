cce7726cf91da91cb8c3137cf61b8810e2debabb
Sgui1234 <danilocpgmeiner@gmail.com>
2021-08-31 16:32:28 -0300

    Atributos

--- OO/Pessoa.py
+++ OO/Pessoa.py
@@ -1,5 +1,6 @@
 class Pessoa():
-    def __init__(self, nome=None, idade=0):
+    def __init__(self, *filhos, nome=None, idade=0):
+        self.filhos = list(filhos)
         self.idade = idade
         self.nome = nome
     def cumprimentar(self,nome=None):
@@ -9,5 +10,9 @@ if __name__ == '__main__':
     name = input("Digite Seu Nome: ")
     age = int(input("Digite Quantos Anos Você Vai Fazer: "))
     p = Pessoa(name,age)
+    fi = Pessoa(nome='Seu Filho')
+    pe = Pessoa(fi, nome=name)
     print(p.cumprimentar(name))
     print(f'{p.nome} Vai Fazer {p.idade} Anos')
+    for i in pe.filhos:
+        print(i.nome)
